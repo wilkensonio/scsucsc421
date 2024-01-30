04ab4301b779388c1a3a334540cb9d8b56f2922a
Wilkenson <tcbwilkenson@gamilcom>
2024-01-29 19:03:53 -0500

    add integer

--- day1/hello.hs
+++ day1/hello.hs
@@ -1,4 +1,15 @@
-main = putStrLn "Hello, World!"
+
+module Main where 
+
+a::Integer -> Integer -> Integer
+a n m = n + m
+
+main :: IO ()
+
+main = do  
+    let result = a 100 2   
+    putStrLn (show result)
+
 
 
 
