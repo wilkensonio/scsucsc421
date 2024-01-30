48314a459ce6177d2c766432eb9668e9b0007ea6
Wilkenson <tcbwilkenson@gamilcom>
2024-01-29 19:13:52 -0500

    xor func

--- day1/hello.hs
+++ day1/hello.hs
@@ -4,11 +4,17 @@ module Main where
 a::Integer -> Integer -> Integer
 a n m = n + m
 
+exOr :: Bool -> Bool -> Bool
+exOr a b = (a || b) && not (a && b)
+
 main :: IO ()
 
 main = do  
     let result = a 100 2   
     putStrLn (show result)
+
+    putStrLn (show (exOr True True))
+
 
 
 
